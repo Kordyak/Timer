e83089f0fe482e0f4a01808f496f5e01b87b4730
kordyak <kordyak@mail.ru>
2026-01-25 15:30:29 +0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.But_play = New System.Windows.Forms.Button()
+        Me.But_reset = New System.Windows.Forms.Button()
+        Me.But_10 = New System.Windows.Forms.Button()
+        Me.But_5 = New System.Windows.Forms.Button()
+        Me.But_1 = New System.Windows.Forms.Button()
+        Me.Label_timer = New System.Windows.Forms.Label()
+        Me.But_1s = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'But_play
+        '
+        Me.But_play.Location = New System.Drawing.Point(143, 73)
+        Me.But_play.Name = "But_play"
+        Me.But_play.Size = New System.Drawing.Size(45, 23)
+        Me.But_play.TabIndex = 1
+        Me.But_play.Text = "play"
+        Me.But_play.UseVisualStyleBackColor = True
+        '
+        'But_reset
+        '
+        Me.But_reset.Location = New System.Drawing.Point(143, 102)
+        Me.But_reset.Name = "But_reset"
+        Me.But_reset.Size = New System.Drawing.Size(45, 23)
+        Me.But_reset.TabIndex = 2
+        Me.But_reset.Text = "reset"
+        Me.But_reset.UseVisualStyleBackColor = True
+        '
+        'But_10
+        '
+        Me.But_10.Location = New System.Drawing.Point(12, 96)
+        Me.But_10.Name = "But_10"
+        Me.But_10.Size = New System.Drawing.Size(45, 23)
+        Me.But_10.TabIndex = 6
+        Me.But_10.Text = "+10m"
+        Me.But_10.UseVisualStyleBackColor = True
+        '
+        'But_5
+        '
+        Me.But_5.Location = New System.Drawing.Point(12, 67)
+        Me.But_5.Name = "But_5"
+        Me.But_5.Size = New System.Drawing.Size(45, 23)
+        Me.But_5.TabIndex = 5
+        Me.But_5.Text = "+5m"
+        Me.But_5.UseVisualStyleBackColor = True
+        '
+        'But_1
+        '
+        Me.But_1.Location = New System.Drawing.Point(12, 38)
+        Me.But_1.Name = "But_1"
+        Me.But_1.Size = New System.Drawing.Size(45, 23)
+        Me.But_1.TabIndex = 4
+        Me.But_1.Text = "+1m"
+        Me.But_1.UseVisualStyleBackColor = True
+        '
+        'Label_timer
+        '
+        Me.Label_timer.AutoSize = True
+        Me.Label_timer.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label_timer.Location = New System.Drawing.Point(31, 9)
+        Me.Label_timer.Name = "Label_timer"
+        Me.Label_timer.Size = New System.Drawing.Size(57, 17)
+        Me.Label_timer.TabIndex = 7
+        Me.Label_timer.Text = "Label1"
+        '
+        'But_1s
+        '
+        Me.But_1s.Location = New System.Drawing.Point(63, 38)
+        Me.But_1s.Name = "But_1s"
+        Me.But_1s.Size = New System.Drawing.Size(45, 23)
+        Me.But_1s.TabIndex = 8
+        Me.But_1s.Text = "+1s"
+        Me.But_1s.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(200, 166)
+        Me.Controls.Add(Me.But_1s)
+        Me.Controls.Add(Me.Label_timer)
+        Me.Controls.Add(Me.But_10)
+        Me.Controls.Add(Me.But_5)
+        Me.Controls.Add(Me.But_1)
+        Me.Controls.Add(Me.But_reset)
+        Me.Controls.Add(Me.But_play)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Timer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Windows.Forms.Timer
+    Friend WithEvents But_play As Button
+    Friend WithEvents But_reset As Button
+    Friend WithEvents But_10 As Button
+    Friend WithEvents But_5 As Button
+    Friend WithEvents But_1 As Button
+    Friend WithEvents Label_timer As Label
+    Friend WithEvents But_1s As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.Windows.Forms
+Imports System.Media
+
+Public Class Form1
+
+
+    Private remaining_seconds As Integer
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Timer1.Interval = 1000
+        Timer1.Enabled = False
+        Update_timer(0)
+
+        ' Включаем обработку клавиш на уровне формы
+        Me.KeyPreview = True
+
+    End Sub
+
+
+
+
+
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+        Select Case e.KeyCode
+            Case Keys.P
+                Play_pause()
+
+            Case Keys.Escape
+                Reset()
+
+        End Select
+
+    End Sub
+
+
+    'PLAY / PAUSE
+    Private Sub Play_pause() Handles But_play.Click
+
+        If But_play.Text = "pause" Then
+
+            Timer1.Stop()
+            But_play.Text = "play"
+            Me.BackColor = SystemColors.Control
+
+        Else
+            If remaining_seconds <= 0 Then
+                If remaining_seconds > 0 Then
+                    Update_timer(0)
+                Else
+                    'MessageBox.Show("Введите корректное время!")
+                    Return
+                End If
+            End If
+
+            Timer1.Start()
+            Me.BackColor = Color.Green
+            But_play.Text = "pause"
+
+        End If
+    End Sub
+
+
+
+    'RESET
+    Private Sub Reset() Handles But_reset.Click
+
+        But_play.Text = "play"
+        Timer1.Stop()
+        Me.BackColor = SystemColors.Control
+        remaining_seconds = 0
+        Update_timer(0)
+
+    End Sub
+
+
+
+
+    'Добавляем время
+    Private Sub But_1s_Click(sender As Object, e As EventArgs) Handles But_1s.Click
+        Update_timer(1)
+    End Sub
+
+    Private Sub But_plus1m_Click(sender As Object, e As EventArgs) Handles But_1.Click
+        Update_timer(60)
+    End Sub
+
+    Private Sub But_plus5m_Click(sender As Object, e As EventArgs) Handles But_5.Click
+        Update_timer(300)
+    End Sub
+
+    Private Sub But_plus10m_Click(sender As Object, e As EventArgs) Handles But_10.Click
+        Update_timer(600)
+    End Sub
+
+
+
+
+
+
+    'Обновление таймера
+    Private Sub Update_timer(second As Integer)
+        remaining_seconds += second
+        Dim minutes As Integer = remaining_seconds \ 60
+        Dim seconds As Integer = remaining_seconds Mod 60
+
+        ' Безопасное обновление Label из потока таймера
+        If Label_timer.InvokeRequired Then
+            Label_timer.Invoke(Sub() Label_timer.Text = $"{minutes:00}:{seconds:00}")
+        Else
+            Label_timer.Text = $"{minutes:00}:{seconds:00}"
+        End If
+    End Sub
+
+
+
+    'ТИК ТАК
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Update_timer(-1)
+
+        If remaining_seconds <= 0 Then
+
+            Me.BackColor = Color.Red
+            Timer1.Stop()
+
+            Task.Run(
+                Sub()
+                    While True
+                        Console.Beep()
+                        Threading.Thread.Sleep(1000)
+
+                        If But_play.Text = "play" Then
+                            Me.BackColor = SystemColors.Control
+                            Exit While
+                        End If
+
+                        Update_timer(-1)
+                    End While
+                End Sub
+            )
+
+        End If
+    End Sub
+
+End Class
